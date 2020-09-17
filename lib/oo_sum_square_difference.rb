ac5c4daf9a69d2207eee7b63bf3113f4c1e61ea9
boom100100 <bsd231@nyu.edu>
2020-09-17 15:20:55 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -6,7 +6,6 @@ class SumSquareDifference
   end
   def difference
     range = (1..@number)
-    # array = range.to_a
     sum_of_squares = range.inject { |sum, n| sum + n ** 2 }
     squared_sum = range.inject { |sum, n| sum + n } ** 2
     squared_sum - sum_of_squares
