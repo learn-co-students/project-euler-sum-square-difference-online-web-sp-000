39a30c0f1eb1093218d1bae6a332abca53e76fb1
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-11 18:07:44 +0100

    Done.

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+def sum_square_difference(num)
+    sum = 0
+    sum_of_squares = 0
+    counter = 1
+
+    until counter > num
+        sum += counter
+        sum_of_squares += (counter ** 2)
+        counter += 1
+    end
+
+    sum ** 2 - sum_of_squares
+end
